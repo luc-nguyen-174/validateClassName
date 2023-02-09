2db81e5651bb6d657d2b709ef51fdf0db86f18ee
luc-nguyen'174 <louiskingvn@gmail.com>
2023-02-09 20:59:00 +0700

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final String REGEX = "^[CAP]+\\d{4}+[GHIKLM]$";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao ten lop");
+        String text = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            System.out.println("Ten lop hop le");
+        }else{
+            System.out.println("Ten lop khong hop le");
+        }
     }
 }
